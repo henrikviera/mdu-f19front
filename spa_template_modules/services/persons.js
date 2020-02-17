7067346783935a14d332a4fc18be5b765ccffb00
Rasmus Cederdorff <rasmus@cederdorff.com>
2020-02-17 08:52:17 +0100

    /*html*/ added.

--- spa_template_modules/services/persons.js
+++ spa_template_modules/services/persons.js
@@ -12,7 +12,7 @@ export default class PersonsService {
   appendPersons(persons) {
     for (let person of persons) { // looping trough all persons
       //creating person data, HTML tags and adding to the DOM, the element #gridPersons
-      document.querySelector("#grid-persons").innerHTML += `
+      document.querySelector("#grid-persons").innerHTML += /*html*/ `
         <article>
         <img src="${person.picture.large}">
         <h4>${person.name.first} ${person.name.last}</h4>
