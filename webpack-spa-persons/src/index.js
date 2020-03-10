89c5ffe4abe14560517d560a99f74a32bdfeb058
Rasmus Cederdorff <rasmus@cederdorff.com>
2020-03-10 20:52:12 +0100

    webpack-spa-persons project added

---
+++ webpack-spa-persons/src/index.js
@@ -0,0 +1,14 @@
+// import your components, pages and services
+import './styles/index.scss';
+import NavBar from "./components/navbar.js";
+import HomePage from "./pages/home.js";
+import PersonsPage from "./pages/persons.js";
+import spaService from "./services/spa.js";
+
+// Declare and init
+let navbar = new NavBar();
+let homePage = new HomePage();
+let personsPage = new PersonsPage();
+
+// init services
+spaService.init();
