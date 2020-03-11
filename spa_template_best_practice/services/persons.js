a97e66047a740ea0b7e8c8cf6952b0ac950d44ec
Rasmus Cederdorff <rasmus@cederdorff.com>
2020-03-11 07:54:46 +0100

    spa_template_best_practice customised

--- spa_template_best_practice/services/persons.js
+++ spa_template_best_practice/services/persons.js
@@ -1,17 +1,9 @@
-import loaderService from "./loader.js";
 class PersonService {
   constructor() {}
 
-  async loadPersons() {
-    try {
-      let response = await fetch("https://randomuser.me/api/?results=9");
-      let jsonData = await response.json();
-      loaderService.show(false);
-      return jsonData.results;
-    } catch (error) {
-      console.log('Error getting persos:', error);
-      loaderService.show(false);
-    }
+  loadPersons() {
+    // let response = await fetch("https://randomuser.me/api/?results=9");
+    // let jsonData = await response.json();
   }
 }
 
